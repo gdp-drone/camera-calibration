1f799e060ee4fd3f98147c6dd5020a522c645415
Vishnu R Menon <vishthemenon@gmail.com>
2019-05-27 15:51:53 +0100

    Complete CVCalibration CLass

---
+++ CVCalibration.h
@@ -0,0 +1,32 @@
+#ifndef OBJECT_TRACKING_CAMERACALIBRATION_H
+#define OBJECT_TRACKING_CAMERACALIBRATION_H
+
+#include <opencv2/core.hpp>
+#include <opencv2/highgui.hpp>
+
+using namespace cv;
+using namespace std;
+
+class CVCalibration {
+  Mat distCoeffs;
+  Mat cameraMatrix = Mat::eye(3, 3, CV_64F);
+  Size chessboardDimensions;
+  float chessboardTileSize;
+  int framesPerSecond = 20;
+  bool calibrated = false;
+  vector<Mat> calibrationImages;
+  vector<vector<Point2f>> allFoundCorners;
+
+  void createKnownBoardPosition(vector<Point3f> &corners);
+  void getCornersFromImages(bool showResults = false);
+  void getCameraMatrices();
+  
+public:
+  CVCalibration(Size chessboardDimensions, float chessboardTileSize);
+  bool startStreamingCalibration(VideoCapture vid, string window);
+  bool isCalibrated();
+  bool saveCalibrationMatrices(string fname);
+  bool loadCalibrationMatrices(string fname);
+};
+
+#endif
